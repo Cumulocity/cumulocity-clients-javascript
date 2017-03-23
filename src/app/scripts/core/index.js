a8e6de962dc59045490882c9448a9219034349d9
Luis Confraria <lconfraria@gmail.com>
2017-03-23 16:36:29 +0000

    deal with gettext module not being preset

--- src/app/scripts/core/index.js
+++ src/app/scripts/core/index.js
@@ -1,8 +1,24 @@
 (function () {
   'use strict';
+
+  checkGettextModule();
+
   angular.module('c8y.core', ['ngRoute', 'angularFileUpload', 'gettext'])
-    .constant('c8yConfig', window.c8yConfig)
-    .constant('gettextCatalog', {
-      getString: function (a) {return a}
-    });
+    .constant('c8yConfig', window.c8yConfig);
+
+  function checkGettextModule() {
+    try {
+      angular.module('gettext');
+    } catch (e) {
+      angular.module('gettext', [])
+        .constant('gettext', identity)
+        .constant('gettextCatalog', {
+          getString: identity
+        });
+    }
+  }
+
+  function identity(str) {
+    return str;
+  }
 })();
